6fa870985bf553ddea83dbb48bbad5b51c892281
SebastianSumi <cspam7780@gmail.com>
2026-08-30 11:13:20 -0500

    Paquete_dev_1

---
+++ saludablemente/src/test/java/pe/edu/upeu/saludablemente/SaludablementeApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upeu.saludablemente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaludablementeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
